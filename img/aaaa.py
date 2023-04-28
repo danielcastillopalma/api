6d148b749bed23f4e3b7fd3a2d3f9eb67d1c3056
danielcastillopalma <d.castillo3@duocuc.cl>
2023-04-28 19:36:44 -0400

    aaaa

--- img/aaaa.py
+++ img/aaaa.py
@@ -207,7 +207,10 @@ tipos = ["Climatización Módulo",
 
 
 while True:
-    print("{"'"idProd": '+str(x)+',\n"nomProd":"'+tipos[x]+'",\n"valorProd":'+str(r.randint(3000,50000))+',\n"descProd": "Texto descriptivo",\n"imgSrc":"https://github.com/danielcastillopalma/api/tree/master/img/'+str(x)+'.bin"}, ')
+    print("{"'"idProd": '+str(x)+',\n"nomProd":"'+tipos[x]+'",\n"valorProd":'+str(r.randint(3000,50000))+',\n"descProd": "Texto descriptivo",\n"imgSrc":"img/api/'+str(x)+'.png"}, ')
+    if x>25:
+      print("{"'"idProd": '+str(x)+',\n"nomProd":"'+tipos[x]+'",\n"valorProd":'+str(r.randint(3000,50000))+',\n"descProd": "Texto descriptivo",\n"imgSrc":"img/api/2.png"}, ')
+     
     x=x+1
     if x==199:
      break
